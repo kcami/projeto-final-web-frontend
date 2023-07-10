f0f2f2e5685f2cb2b604173f639e9fad383683e8
Camila M Reno <camilamreno@gmail.com>
2023-07-10 14:43:25 -0300

    fix: :bug: Corrigindo os dados da chamada de exemplo da api de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,47 +8,50 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Image from "react-bootstrap/Image";
-import { authContext } from '@/contexts/authenticationContext';
+import { authContext } from "@/contexts/authenticationContext";
 import Link from "next/link";
 import useAuth from "@/services/authenticationRequests";
-
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const {token, setToken} = authContext();
-  const {actions} = useAuth();
-  
+  const { token, setToken } = authContext();
+  const { actions } = useAuth();
+
   useEffect(() => {
-    actions.login({medical_register: "string", password: "string", role: "string"})
-  })
+    actions.login({
+      medical_register: "CRM/SP 123456",
+      password: "12345678",
+      role: "MEDICO_UTI",
+    });
+  });
   console.log(token);
   //const [showPassword, setShowPassword] = useState(false);
   //const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   function handleEmailChange(event: ChangeEvent<HTMLInputElement>): void {
     setEmail(event.target.value);
-    console.log(email)
+    console.log(email);
   }
   function handlePasswordChange(event: ChangeEvent<HTMLInputElement>): void {
     setPassword(event.target.value);
-    console.log(password)
+    console.log(password);
   }
-  
+
   return (
     <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
       <Container className={`${styles.container} w-75 shadow`}>
-        <Row className="pl-5">
+        <Row className='pl-5'>
           <Col className='p-5'>
             <div className='d-flex justify-content-center'>
               <Image width={"150px"} src='./images/logo.svg' />
             </div>
-            
+
             <Row>
-              <Form className="d-flex justify-content-center pb-3">
+              <Form className='d-flex justify-content-center pb-3'>
                 <Form.Check
                   inline
                   label='Solicitar vaga'
@@ -66,13 +69,18 @@ export default function Login() {
               </Form>
               <Form>
                 <Form.Label>Identificador (CRM/COREN)</Form.Label>
-                <Form.Control type='text' id='user' className="shadow-sm" onChange={handlePasswordChange} />
+                <Form.Control
+                  type='text'
+                  id='user'
+                  className='shadow-sm'
+                  onChange={handlePasswordChange}
+                />
                 <Form.Label>Senha</Form.Label>
                 <Form.Control
                   type='password'
                   id='password'
                   aria-describedby='passwordHelpBlock'
-                  className="shadow-sm"
+                  className='shadow-sm'
                   onChange={handleEmailChange}
                 />
               </Form>
@@ -81,13 +89,18 @@ export default function Login() {
                   Esqueceu sua senha?
                 </a>
               </p>
-              <Button variant='primary' className="m-2 shadow">
+              <Button variant='primary' className='m-2 shadow'>
                 <p className='text-white m-0'>Login</p>
               </Button>
             </Row>
-            <p>Ainda não tem conta? <Link href="/cadastro">Faça seu cadastro!</Link></p>
+            <p>
+              Ainda não tem conta?{" "}
+              <Link href='/cadastro'>Faça seu cadastro!</Link>
+            </p>
           </Col>
-          <Col className={`p-0 pt-4 pb-4 ${styles.imgContainer} d-xl-flex justify-content-center d-none d-xl-block `}>
+          <Col
+            className={`p-0 pt-4 pb-4 ${styles.imgContainer} d-xl-flex justify-content-center d-none d-xl-block `}
+          >
             <Image src='./images/login.svg' />
           </Col>
         </Row>
