3f68f42b1563d356ec4adaf7d252c24a90ebaff7
Camila M Reno <camilamreno@gmail.com>
2023-07-13 18:39:23 -0300

    feat: :sparkles: Adicionando o serviço de autenticação, o contexto do usuário e a tela de login

--- src/contexts/authenticationContext.tsx
+++ src/contexts/authenticationContext.tsx
@@ -1,8 +1,10 @@
+import router from 'next/router';
 import { createContext, useState, useContext } from 'react';
 
 interface ContextAuth {
     token: string;
     setToken: (token: string) => void;
+    routes: () => void;
 }
 
 const AuthContext = createContext<ContextAuth>({} as ContextAuth);
@@ -13,9 +15,29 @@ interface AuthContextProps {
 
 export function AuthProvider({ children }: AuthContextProps) {
     const [token, setToken] = useState("tokenExemplo");
+
+    function routes() {
+      if (token) {
+        const userType = token;
+        console.log(token)
+        const jwt = require("jsonwebtoken");
+  
+        const decodedToken = jwt.decode(token);
+        const userRole = decodedToken.role;
+        if (userRole === "MEDICO_UTI") {
+          router.push("/home");
+        } else if (userRole === "MEDICO_GERAL") {
+          router.push("/homeGeral");
+        } else if (userRole === "ENFERMEIRO") {
+          router.push("/homeEnfermeiro");
+        }
+      } else {
+        router.push("/login");
+      }
+    }
   
     return (
-      <AuthContext.Provider value={{ token, setToken }}>
+      <AuthContext.Provider value={{ token, setToken, routes }}>
         {children}
       </AuthContext.Provider>
     );
--- src/services/authenticationRequests.tsx
+++ src/services/authenticationRequests.tsx
@@ -1,66 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import axios from "axios";
 import { AuthLogin, AuthRaw, AuthCreate } from "../@types/auth";
 import { AlertError } from "../components/AlertError";
 import { AlertSucess } from "../components/AlertSucess";
+import { authContext } from "@/contexts/authenticationContext";
+import router from "next/router";
 const baseUrl = "http://localhost:3333";
 
-
 export interface userActions {
-    // get: (roomID?: string) => Promise<void>;
-    // put: ( roomID: string, putObject: any ) => Promise<void>;
-    login: (body: AuthLogin) => Promise<void>;
-    postUser: (body: AuthCreate) => Promise<void>;
-    // delete: (roomID: string) => Promise<void>;
+  login: (body: AuthLogin) => Promise<void>;
+  postUser: (body: AuthCreate) => Promise<void>;
 }
 
-export const useAuth = (): {loading: boolean, actions: userActions} => {
-    const [loading, setLoading] = useState(true);
+export const useAuth = (): { loading: boolean; actions: userActions } => {
+  const [loading, setLoading] = useState(true);
+  const { token, setToken, routes } = authContext();
 
-    async function login( body: AuthLogin ): Promise<void> {
-        await new Promise(async (resolve) => {
-            try {
-                setLoading(true);
-                const response = await axios.post(`${baseUrl}/authenticate`,
-                    body
-                );
-                if (response.data) {
-                    //setRoom(response.data);
-                    setLoading(false);
-                    AlertSucess("Login realizado com sucesso!")
-                }
-            } catch (err) {
-                setLoading(false);
-                AlertError("Ocorreu um erro no login!")
-            }
-        });
-    }
+  async function login(body: AuthLogin): Promise<void> {
+    await new Promise(async (resolve) => {
+      try {
+        setLoading(true);
+        const response = await axios.post(`${baseUrl}/authenticate`, body);
+        if (response.data) {
+          setLoading(false);
+          setToken(response.data.token);
+          console.log(token);
+          console.log("Oi")
+          localStorage.setItem("token", response.data.token);
+          console.log("certo")
+          routes();
+          AlertSucess("Login realizado com sucesso!");
+        }
+      } catch (err) {
+        setLoading(false);
+        console.log("erro")
+        console.log(err);
+        AlertError("Ocorreu um erro no login!");
+      }
+    });
+  }
 
-    async function postUser(
-        body: AuthCreate
-    ) {
-        await new Promise(async (resolve) => {
-            try {
-                setLoading(true);
-                const response = await axios.post(`${baseUrl}/create-room`,
-                    body
-                );
-                if (response.data) {
-                    //setRoom(response.data);
-                    setLoading(false);
-                    AlertSucess("Solicitação de usuário criada com sucesso!")
-                }
-            } catch (err) {
-                setLoading(false);
-                AlertError("Não foi possível criar a solicitação de usuário!")
-            }
-        });
-    }
+  const signOut = async () => {
+    setLoading(true);
+    localStorage.removeItem("token");
+    setToken("");
+    setLoading(false);
+    router.push("/");
+  };
 
-    const actions = {login, postUser};
 
-    return {loading, actions}
+  async function postUser(body: AuthCreate) {
+    await new Promise(async (resolve) => {
+      try {
+        console.log(body);
+        setLoading(true);
+        const response = await axios.post(`${baseUrl}/register-collaborator`, body);
+        if (response.data) {
+          setLoading(false);
+          AlertSucess("Solicitação de usuário criada com sucesso!");
+        }
+      } catch (err) {
+        setLoading(false);
+        // AlertError("Não foi possível criar a solicitação de usuário!");
+      }
+    });
+  }
 
+  const actions = { login, postUser };
+
+  return { loading, actions };
 };
 
 export default useAuth;
