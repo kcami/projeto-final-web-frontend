281f9c9a8254527ec4ecbc61a1d47a2d39c07472
Camila M Reno <camilamreno@gmail.com>
2023-07-13 23:26:34 -0300

    fix: :lipstick: Ajustando as estilizações da página home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,61 @@
-import {Navbar,Container,NavbarBrand, Image, Nav, ListGroup, NavLink, Button} from 'react-bootstrap';
-import styles from '../styles/page.module.css';
-import headerStyle from '../styles/header.module.css';
-import Legend from '../components/Legend';
+import {
+  Navbar,
+  Container,
+  NavbarBrand,
+  Image,
+  Nav,
+  ListGroup,
+  NavLink,
+  Button,
+} from "react-bootstrap";
+import styles from "../styles/page.module.css";
+import headerStyle from "../styles/header.module.css";
+import Legend from "../components/Legend";
 
-export default function Header(props: { linkText: string, pageType: number, legendType: number, legendText: string }){
-
-    return (
-        <Navbar expand="lg" fixed="top" className={`${headerStyle.navbar}`} bg="bluePrimary">
-            <Container className={`${styles.container}`}>
-                <NavbarBrand >
-                    <Image width={"150px"} src='./images/Rectangle_2.png' />
-                </NavbarBrand>
-                <Nav className={`${headerStyle.navbar_nav}`}>
-                    {
-                    props.pageType == 1 && <NavLink className={`${headerStyle.navLink}`} href="/profile">
-                        <Image src='./images/account_circle.png'/>
-                        Perfil</NavLink>
-                    }
-                    {
-                    props.pageType == 2 && <NavLink className={`${headerStyle.navLink}`} href="/home">
-                        <Image src='./images/home.png'/>
-                        Home
-                        </NavLink>
-                    }
-                    <NavLink className={`${headerStyle.navLink}`} href="#">
-                        <Image src='./images/exit_to_app.png' />
-                        Sair</NavLink>
-                    {
-                    (props.linkText != '' && props.pageType == 1) && <NavLink href="/lista"><Button variant="blueButtom" className={`${styles.blue_button}`}>{props.linkText}</Button></NavLink>
-                    }
-                </Nav>
-            </Container>
-            <Legend pageType={props.legendType} componentText={props.legendText}/>
-        </Navbar>
-        )
+export default function Header(props: {
+  linkText: string;
+  pageType: number;
+  legendType: number;
+  legendText: string;
+}) {
+  return (
+    <Navbar
+      expand='lg'
+      fixed='top'
+      className={`${headerStyle.navbar}`}
+      bg='bluePrimary'
+    >
+      <Container className={`${styles.container}`}>
+        <NavbarBrand>
+          <Image width={"150px"} src='./images/Rectangle_2.png' />
+        </NavbarBrand>
+        <Nav className={`${headerStyle.navbar_nav}`}>
+          {props.pageType == 1 && (
+            <NavLink className={`${headerStyle.navLink} d-flex justify-content-center`} href='/profile'>
+              <Image src='./images/account_circle.png' />
+              Perfil
+            </NavLink>
+          )}
+          {props.pageType == 2 && (
+            <NavLink className={`${headerStyle.navLink} d-flex justify-content-center`} href='/home'>
+              <Image src='./images/home.png' />
+              Home
+            </NavLink>
+          )}
+          <NavLink className={`${headerStyle.navLink} d-flex justify-content-center`} href='#'>
+            <Image src='./images/exit_to_app.png' />
+            Sair
+          </NavLink>
+          {props.linkText != "" && props.pageType == 1 && (
+            <NavLink href='#'>
+              <Button variant='blueButtom' className={`${styles.blue_button}`}>
+                {props.linkText}
+              </Button>
+            </NavLink>
+          )}
+        </Nav>
+      </Container>
+      <Legend pageType={props.legendType} componentText={props.legendText} />
+    </Navbar>
+  );
 }
--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,43 +1,52 @@
-import styles from '../styles/page.module.css';
-import {Container,ListGroup} from 'react-bootstrap';
-import styleLegenda from '../styles/legend.module.css';
-export default function Legend(props: {pageType: number, componentText: string}){
-    const legenda = [
-        {
-          color: styles.green,
-          texto: "UTIs disponiveis"
-        },
-        {
-          color: styles.yellow,
-          texto: "UTIs em limpeza"
-        },
-        {
-          color: styles.red,
-          texto: "UTIs ocupadas"
-        }
-      ]
-    return(
-        <>
-        { props.pageType != 0 &&
-        <Container className={`${styleLegenda.legenda} w-100 bg-blueSecondary`} >
-                {props.pageType == 1 && <ListGroup className={`${styleLegenda.listGroup} w-100`} variant='blueSecondary'>
-                    {legenda.map((element, i) => {
-                        return (
+import styles from "../styles/page.module.css";
+import { Container, ListGroup } from "react-bootstrap";
+import styleLegenda from "../styles/legend.module.css";
+export default function Legend(props: {
+  pageType: number;
+  componentText: string;
+}) {
+  const legenda = [
+    {
+      color: styles.green,
+      texto: "UTIs disponiveis",
+    },
+    {
+      color: styles.yellow,
+      texto: "UTIs em limpeza",
+    },
+    {
+      color: styles.red,
+      texto: "UTIs ocupadas",
+    },
+  ];
+  return (
+    <>
+      {props.pageType != 0 && (
+        <Container className={`${styleLegenda.legenda} w-100 bg-blueSecondary`}>
+          {props.pageType == 1 && (
+            <ListGroup
+              className={`${styleLegenda.listGroup} w-100`}
+              variant='blueSecondary'
+            >
+              {legenda.map((element, i) => {
+                return (
+                  <ListGroup.Item
+                    key={i}
+                    className={`${styleLegenda.itemLegenda}`}
+                  >
+                    <div
+                      className={`${element.color}`}
+                    ></div>
+                    {element.texto}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          )}
 
-                            <ListGroup.Item key={i} className={`${styleLegenda.itemLegenda}`}>
-
-                                <div className={`${element.color}`}></div>
-                                {element.texto}
-
-                            </ListGroup.Item>
-
-                        )
-                    })}</ListGroup>}
-
-                    {(props.pageType == 2) &&  <p>{props.componentText}</p>}
-                
-            </Container>
-        }
-        </>
-    )
+          {props.pageType == 2 && <p>{props.componentText}</p>}
+        </Container>
+      )}
+    </>
+  );
 }
